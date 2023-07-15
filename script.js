e1efeb0e7d10abf2882f8be3dd36f0ac4380b8c1
Cuckss <akshatchaturvedi60@gmail.com>
2023-07-15 17:09:59 +0530

    project july 1

--- script.js
+++ script.js
@@ -7,21 +7,40 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  arr.map((student)=>{
+    console.log(student)
+  })
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((student)=>{
+    console.log(student);
+  })
 }
 
 function addData() {
-  //Write your code here, just console.log
+ const obj={id:4,name:"shiva",age:"21",marks:90}
+ arr.push(obj)
+ console.log(arr);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  arr.filter((student)=>{
+    if(student.marks<36){
+      arr.pop(student);
+    }
+  })
+  arr.forEach((student)=>{
+    console.log(student);
+  })
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr=[{id:5,name:"akshat",age:"22",marks:72},
+  {id:6,name:"john",age:"24",marks:95},
+  {id:7,name:"chris",age:"25",marks:60}]
+  const concatArr=arr.concat(newArr);
+  arr=concatArr;
+  console.log(arr)
 }
